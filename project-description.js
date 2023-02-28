dfa5db3af7a9065e24b8f70981fa4e6f88257618
Sabine M. Heyne <hello@smheyne.de>
2023-02-28 21:49:11 +0200

    Implement projects directly on website

--- project-description.js
+++ project-description.js
@@ -1,14 +1,24 @@
 
-function toggleDescription(projectDescriptionId){
+function toggleDescription(projectDescriptionId, button, projectDivId){
   const descriptionDiv = document.getElementById(projectDescriptionId);
+  const projectDiv = document.getElementById(projectDivId);
   /* Übergeben der ID aus dem Button, der dort im html in der Funktion gesetzt wird */
 
     if (window.getComputedStyle(descriptionDiv).visibility === "collapse") {
 
         showDescription(projectDescriptionId);
+        button.innerText = "Show less";
+        projectDiv.classList.remove("drittel");
+        projectDiv.classList.add("zweidrittel");
+        projectDiv.style.background = "#FFD96B66";
+
     }
     else {
         hideDescription(projectDescriptionId);
+        button.innerText = "Read more";
+        projectDiv.classList.add("drittel");
+        projectDiv.classList.remove("zweidrittel");
+        projectDiv.style.background = "none";
     }
 }
 
@@ -16,7 +26,7 @@ function showDescription(projectDescriptionId) {
   const descriptionDiv = document.getElementById(projectDescriptionId);
 
   descriptionDiv.style.visibility = "visible";
-  descriptionDiv.style.display = "flex";
+  descriptionDiv.style.display = "block";
 }
 
 function hideDescription(projectDescriptionId) {

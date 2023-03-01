1d5e729eac5c4d11edac52f3214798f35e8112f9
Sabine M. Heyne <hello@smheyne.de>
2023-03-01 19:20:24 +0200

    hide/show video thumbnail when video toggled

--- videos.js
+++ videos.js
@@ -1,35 +1,41 @@
 /* For the show Video Button on each thumbnail*/
-function toggleVideo(videoElementId){
+function toggleVideo(videoElementId, thumbnailToggleButton){
     const videoDiv = document.getElementById(videoElementId);
 
     if (window.getComputedStyle(videoDiv).visibility === "collapse") {
 
         showVideo(videoElementId);
+        thumbnailToggleButton.style.visibility = "collapse";
+        thumbnailToggleButton.style.display = "none";
     }
     else {
         hideVideo(videoElementId);
+        thumbnailToggleButton.style.visibility = "visible";
+        thumbnailToggleButton.style.display = "block";
     }
 }
 
-/*Needs to automatically close all other videos when a new one is shown*/
-
-function showVideo(videoElementId){
-    const videoDiv = document.getElementById(videoElementId);
-    const videoElement = videoDiv.getElementsByTagName("video")[0];
+function showVideo(videoContainerId){
+    const videoContainer = document.getElementById(videoContainerId);
+    const videoElement = videoContainer.getElementsByTagName("video")[0];
 
     videoElement.currentTime = 0;
-    videoDiv.style.visibility = "visible";
-    videoDiv.style.display = "block";
+    videoContainer.style.visibility = "visible";
+    videoContainer.style.display = "block";
     videoElement.play();
 }
 
 /*For the close Button on each video*/
 
-function hideVideo(videoElementId){
-    const videoDiv = document.getElementById(videoElementId);
-    const videoElement = videoDiv.getElementsByTagName("video")[0];
-
-    videoDiv.style.visibility = "collapse";
-    videoDiv.style.display = "none";
+function hideVideo(videoContainerId, thumbnailShowID){
+    const videoContainer = document.getElementById(videoContainerId);
+    const videoElement = videoContainer.getElementsByTagName("video")[0];
+    
+    videoContainer.style.visibility = "collapse";
+    videoContainer.style.display = "none";
     videoElement.pause();
+
+    const thumbnailToggleVideo = document.getElementById(thumbnailShowID);
+    thumbnailToggleVideo.style.visibility = "visible";
+    thumbnailToggleVideo.style.display = "inline";
 }
